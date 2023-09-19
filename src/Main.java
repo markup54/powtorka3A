becb5028d43889d92727dd8ba2e82b921915f21b
markup54 <marzenakupka@tlen.pl>
2023-09-19 11:27:09 +0200

    wylosowane

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
+
+    public static ArrayList<Integer> losujLiczby(){
+        ArrayList<Integer> wylosowane = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            wylosowane.add((int)(Math.random()*51+100));
+        }
+        return wylosowane;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> wylosowaneLiczby = losujLiczby();
+        System.out.println(wylosowaneLiczby);
     }
 }
