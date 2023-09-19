46b870cf9f0d579bb9a724151f631cd0caac23be
markup54 <marzenakupka@tlen.pl>
2023-09-19 12:15:25 +0200

    palindromiczna i najczesciej wystepujaca

--- src/Main.java
+++ src/Main.java
@@ -24,11 +24,42 @@ public class Main {
         return zbior;
 
     }
+    public static void ileRazy(ArrayList<Integer> wszystkieLiczby){
+        HashSet<Integer> bezpowt = zapiszDoZbioru(wszystkieLiczby);
+        ArrayList<Integer>ilePowtorzen = new ArrayList<>();
+        for (int i = 0; i < 151; i++) {
+            ilePowtorzen.add(0);
+        }
+        for (Integer liczba:bezpowt
+             ) {
+            for (Integer liczbapowt: wszystkieLiczby
+                 ) {
+                if(liczba == liczbapowt){
+                    ilePowtorzen.set(liczba,ilePowtorzen.get(liczba)+1);
+                }
+            }
+        }
+        int maksimum = 0;
+        for (int ile:ilePowtorzen) {
+            if(maksimum<ile)
+                maksimum = ile;
+        }
+        System.out.println(ilePowtorzen);
+        System.out.println(maksimum);
+        System.out.println(ilePowtorzen.indexOf(maksimum));
+    }
     public static void main(String[] args) {
         ArrayList<Integer> wylosowaneLiczby = losujLiczby();
         System.out.println(wylosowaneLiczby);
         System.out.println(sumaZListy(wylosowaneLiczby));
         HashSet<Integer> liczbyBezPowtorzen = zapiszDoZbioru(wylosowaneLiczby);
         System.out.println("liczb bez powtórzeń "+liczbyBezPowtorzen.size());
+        ileRazy(wylosowaneLiczby);
+        for (Integer liczba: wylosowaneLiczby
+             ) {
+            if(liczba % 10 == 1){
+                System.out.println(liczba);
+            }
+        }
     }
 }
